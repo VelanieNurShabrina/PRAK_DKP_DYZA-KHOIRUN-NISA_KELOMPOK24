0774c4bcaf53804399a82e3e0e00c7518c6f5985
VelanieNurShabrina <102465235+VelanieNurShabrina@users.noreply.github.com>
2022-03-27 21:45:39 +0700

    Add files via upload

---
+++ method.py
@@ -0,0 +1,16 @@
+class login_method:
+    def __init__(self, list1, list2, list3, list4) :
+        self.list1 = list1
+        self.list2 = list2
+        self.list3 = list3
+        self.list4 = list4
+
+    def awal(self):
+        print("===== SELAMAT DATANG DI IBUKOST =====")
+        print()
+        print("         KOST YANG TERSEDIA      \n ")
+        print(f'1. {self.list1} (JAKARTA)\n2. {self.list2} (SEMARANG)\n3. {self.list3} (BALI)\n4. {self.list4} (TANGGERANG)\n')
+
+
+        
+
